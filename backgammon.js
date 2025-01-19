94a79816220f90bc54ec7f02dedbc0c3453f3029
GTucker2 <griffinavtucker@gmail.com>
2025-01-18 23:39:35 -0800

    feature[backgammon]: restructuring with point class

--- backgammon.js
+++ backgammon.js
@@ -1,9 +1,10 @@
-function startGame() {
-    gameArea.start();
-}
-
 const TOTAL_POINTS = 24;
 const BAR_WIDTH = 20;
+const CANVAS_HEIGHT = 270;
+const CANVAS_WIDTH = 480;
+const POINTS_WIDTH = (CANVAS_WIDTH - BAR_WIDTH) / (TOTAL_POINTS / 2);
+const POINTS_HEIGHT = CANVAS_HEIGHT / 2;
+const CHECKER_RADIUS = POINTS_HEIGHT/10;
 const STYLE = {
     POINTS_COLOR_A : "brown",
     POINTS_COLOR_B : "red",
@@ -11,69 +12,110 @@ const STYLE = {
     CHECKER_COLOR_B : "white"
 }
 
+const INITIAL_WEIGHTING = [2,0,0,0,0,5,
+                           0,3,0,0,0,5,
+                           5,0,0,0,3,0,
+                           5,0,0,0,0,2,
+];
+
+const INITIAL_POINTS_STATE = [1,0,0,0,0,2,
+                              0,2,0,0,0,1,
+                              2,0,0,0,1,0,
+                              1,0,0,0,0,2,
+];
+
+var points = [];
+
+function startGame() {
+    initGame();
+    gameArea.start();
+}
+
+function initGame() {
+    for(let i=1; i <= TOTAL_POINTS; i++) {
+        weight = INITIAL_WEIGHTING[i];
+        state = INITIAL_POINTS_STATE[i];
+        points.push(new Point(i, weight, state)); 
+    }
+}
+
 var gameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = CANVAS_WIDTH;
+        this.canvas.height = CANVAS_HEIGHT;
         this.context = this.canvas.getContext("2d");
-        this.drawGameArea();
+        this.update();
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
-    drawGameArea : function() {
-        const CANVAS_HEIGHT = this.canvas.height;
-        const POINTS_WIDTH = (this.canvas.width - BAR_WIDTH) / (TOTAL_POINTS / 2);
-        const POINTS_HEIGHT = CANVAS_HEIGHT / 2;
-        const CHECKER_RADIUS = POINTS_HEIGHT/10;
-        for(let i = 0; i < TOTAL_POINTS/2; i++) {
-            var x = i * POINTS_WIDTH;
-            if(i >= TOTAL_POINTS/4) {
-                x += BAR_WIDTH;
+    update : function() {
+        this.clear();
+        points.forEach(point => {
+            point.draw();
+            for(let i=0; i<point.weight; i++) {
+
             }
-            if(i % 2 == 0) {
-                drawPoint(POINTS_WIDTH, POINTS_HEIGHT, x, 0, STYLE.POINTS_COLOR_A);
-                drawPoint(POINTS_WIDTH, -POINTS_HEIGHT, x, CANVAS_HEIGHT, STYLE.POINTS_COLOR_B)
-                } else {
-                drawPoint(POINTS_WIDTH, POINTS_HEIGHT, x, 0, STYLE.POINTS_COLOR_B);
-                drawPoint(POINTS_WIDTH, -POINTS_HEIGHT, x, CANVAS_HEIGHT, STYLE.POINTS_COLOR_A)
-            }
-            drawChecker(CHECKER_RADIUS, x + POINTS_WIDTH/2, POINTS_HEIGHT/9, "black");
-            drawChecker(CHECKER_RADIUS, x + POINTS_WIDTH/2, (POINTS_HEIGHT*2) - (POINTS_HEIGHT/9), "black");  
-        }
-    },
-    drawPoint : function(width, height, x, y, color) {
+        });
+    }
+}
+
+function Point(index, weight, state) {
+    _offset = (index <= TOTAL_POINTS/4) || (index > 18 ) ? BAR_WIDTH : 0;
+    _x_white = (((12)-(index)) * POINTS_WIDTH) + _offset;
+    _x_black = ((index - 13) * POINTS_WIDTH) + _offset;
+    this.color = index % 2 == 0 ? STYLE.POINTS_COLOR_A : STYLE.POINTS_COLOR_B;
+    this.x = index <= TOTAL_POINTS/2 ? _x_white : _x_black;
+    this.y = index <= TOTAL_POINTS/2 ? CANVAS_HEIGHT : 0;
+    this.height = (index <= TOTAL_POINTS/2) ? -POINTS_HEIGHT : POINTS_HEIGHT;
+    this.width = POINTS_WIDTH;
+    this.index = index;
+    this.weight = weight;
+    this.state = state; 
+    this.draw = function() {
         ctx = gameArea.context; 
-        ctx.fillStyle = color;
+        ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.moveTo(x,y);
-        ctx.lineTo(x+width,y);
-        ctx.lineTo(x+(width/2),y+height);
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x + this.width, this.y);
+        ctx.lineTo(this.x + (this.width/2), this.y + this.height);
         ctx.closePath();    
-        ctx.fillStyle = color;
-        ctx.fill();
-    },
-    drawChecker : function(radius, x, y, color) {
-        ctx = gameArea.context; 
-        ctx.fillStyle = color;
-        ctx.beginPath();
-        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = this.color;
         ctx.fill();
     }
 }
 
-function drawPoint(width, height, x, y, color) {
-    ctx = gameArea.context; 
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(x,y);
-    ctx.lineTo(x+width,y);
-    ctx.lineTo(x+(width/2),y+height);
-    ctx.closePath();    
-    ctx.fillStyle = color;
-    ctx.fill();
+class Checker extends Component {
+    constructor(index, color) {
+        this.width = CHECKER_RADIUS * 2;
+        this.height = CHECKER_RADIUS * 2; 
+        this.color = color;
+        this.index = this.setIndex(index);
+        this.draw = function() {
+            ctx = gameArea.context;
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(x, y, CHECKER_RADIUS, 0, 2 * Math.PI, false);
+            ctx.fill();
+        }
+    }
+    getIndex() { return this.index; }
+    setIndex(index) {
+        widthOffset = 0;
+        heightOffset = 0;
+        const POSITION_OFFSET = POINTS_HEIGHT/9;
+        if (1 <= index <= TOTAL_POINTS/4)
+            widthOffset = BAR_WIDTH;
+        if (TOTAL_POINTS - ((TOTAL_POINTS/4)-1) <= index <= TOTAL_POINTS)
+            widthOffset = BAR_WIDTH;
+        if (index > TOTAL_POINTS/2)
+            heightOffset = POINTS_HEIGHT*2;
+        this.setX((index * POINTS_WIDTH) + widthOffset);
+        this.setY(heightOffset - POSITION_OFFSET); 
+        this.index = index;
+    }
 }
 
 function drawChecker(radius, x, y, color) {
